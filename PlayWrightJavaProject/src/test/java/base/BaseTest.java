7eb7ec2df22e3be6ae704a4c95d402d813979151
User <User@LAPTOP-HOIPKMNS>
2025-12-28 15:09:46 +0530

    updated

--- PlayWrightJavaProject/src/test/java/base/BaseTest.java
+++ PlayWrightJavaProject/src/test/java/base/BaseTest.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
@@ -30,21 +31,54 @@ public class BaseTest {
 	protected Logger log = LoggerUtil.getLogger(getClass());
 	protected ExtentReports extent;
 	public ExtentTest test;
-	
-	
+
+	@Parameters("browser")
 	@BeforeMethod
-	public void setUp(Method method) {
+	public void setUp(String browserName, Method method) {
 		// Initialize ExtentReports
 		extent = ExtentManager.getInstance();
 		test = extent.createTest(method.getName());
 		test.info("Starting test: " + method.getName());
-		
+
 		playwright = Playwright.create();
-		browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setArgs(List.of("--start-maximized")));
+		BrowserType.LaunchOptions options = new BrowserType.LaunchOptions().setHeadless(false);
+
+		switch (browserName.toLowerCase()) {
+		case "chrome":
+			log.info("Launching Chrome browser");
+			test.info("Launching Chrome browser");
+			browser = playwright.chromium().launch(options.setChannel("chrome").setArgs(List.of("--start-maximized")));
+			log.info("Chrome browser launched");
+			test.info("Chrome browser launched");
+			break;
+
+		case "edge":
+			log.info("Launching Edge browser");
+			test.info("Launching Edge browser");
+			browser = playwright.chromium().launch(options.setChannel("msedge").setArgs(List.of("--start-maximized")));
+			log.info("Edge browser launched");
+			test.info("Edge browser launched");
+			break;
+
+		case "firefox":
+			log.info("Launching Firefox browser");
+			test.info("Launching Firefox browser");
+			browser = playwright.firefox().launch(options.setArgs(List.of("--start-maximized")));
+			log.info("Firefox browser launched");
+			test.info("Firefox browser launched");
+			break;
+
+		default:
+			log.error("Unsupported browser: " + browserName + ". Launching Chrome as default.");
+			browser = playwright.chromium().launch(options.setChannel("chrome").setArgs(List.of("--start-maximized")));
+			break;
+		}
+
+		// Create a new browser context and page
 		context = browser.newContext(new Browser.NewContextOptions().setViewportSize(null));
 		page = context.newPage();
 		log.info("Browser launched successfully");
-		
+
 		ObjectRepository objRepo = new ObjectRepository();
 		// Navigate to base URL
 		page.navigate(objRepo.baseURL);
@@ -54,16 +88,16 @@ public class BaseTest {
 	@AfterMethod
 	public void tearDowon(ITestResult result) {
 		log.info("Closing the browser");
-		
-		if(result.getStatus() == ITestResult.FAILURE) {
+
+		if (result.getStatus() == ITestResult.FAILURE) {
 			test.fail("Test Failed: " + result.getThrowable());
 			ScreenshotUtil.takeScreenshot(page, test);
-		} else if(result.getStatus() == ITestResult.SUCCESS) {
+		} else if (result.getStatus() == ITestResult.SUCCESS) {
 			test.pass("Test Passed");
-		} else if(result.getStatus() == ITestResult.SKIP) {
+		} else if (result.getStatus() == ITestResult.SKIP) {
 			test.skip("Test Skipped: " + result.getThrowable());
 		}
-		
+
 		extent.flush();
 
 		if (playwright != null)
